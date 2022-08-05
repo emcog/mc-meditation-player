48d5bad340465c97f674740b49fb97114468704a
Robin <robin.leonard@gmail.com>
2022-08-05 13:41:10 +0100

    updating

--- .svelte-kit/runtime/env/static/private.js
+++ .svelte-kit/runtime/env/static/private.js
@@ -1,4 +1,7 @@
 // this file is generated — do not edit it
+/** @type {import('$env/static/private').TERM_PROGRAM} */
+export const TERM_PROGRAM = "Apple_Terminal";
+
 /** @type {import('$env/static/private').NODE} */
 export const NODE = "/usr/local/bin/node";
 
@@ -20,11 +23,14 @@ export const TMPDIR = "/var/folders/jp/vkqt6_g971l73lwt2jvf94zh0000gn/T/";
 /** @type {import('$env/static/private').npm_config_global_prefix} */
 export const npm_config_global_prefix = "/usr/local";
 
+/** @type {import('$env/static/private').TERM_PROGRAM_VERSION} */
+export const TERM_PROGRAM_VERSION = "440";
+
 /** @type {import('$env/static/private').COLOR} */
 export const COLOR = "1";
 
 /** @type {import('$env/static/private').TERM_SESSION_ID} */
-export const TERM_SESSION_ID = "82986f2a-c0e7-43f5-ae16-80b65f78f2e5";
+export const TERM_SESSION_ID = "F0AF53D1-DD7C-4FEB-88E2-F0D941D93F51";
 
 /** @type {import('$env/static/private').npm_config_noproxy} */
 export const npm_config_noproxy = "";
@@ -35,14 +41,8 @@ export const npm_config_local_prefix = "/Users/robin/Sites/mc-meditation-player"
 /** @type {import('$env/static/private').ZSH} */
 export const ZSH = "/Users/robin/.oh-my-zsh";
 
-/** @type {import('$env/static/private').__INTELLIJ_COMMAND_HISTFILE__} */
-export const __INTELLIJ_COMMAND_HISTFILE__ = "/Users/robin/Library/Caches/JetBrains/WebStorm2021.2/terminal/history/mc-meditation-player-history";
-
 /** @type {import('$env/static/private').USER} */
 export const USER = "robin";
-
-/** @type {import('$env/static/private').COMMAND_MODE} */
-export const COMMAND_MODE = "unix2003";
 
 /** @type {import('$env/static/private').npm_config_globalconfig} */
 export const npm_config_globalconfig = "/usr/local/etc/npmrc";
@@ -56,9 +56,6 @@ export const __CF_USER_TEXT_ENCODING = "0x1F5:0:2";
 /** @type {import('$env/static/private').npm_execpath} */
 export const npm_execpath = "/usr/local/lib/node_modules/npm/bin/npm-cli.js";
 
-/** @type {import('$env/static/private').LOGIN_SHELL} */
-export const LOGIN_SHELL = "1";
-
 /** @type {import('$env/static/private').PAGER} */
 export const PAGER = "less";
 
@@ -66,16 +63,16 @@ export const PAGER = "less";
 export const LSCOLORS = "Gxfxcxdxbxegedabagacad";
 
 /** @type {import('$env/static/private').PATH} */
-export const PATH = "/Users/robin/Sites/mc-meditation-player/node_modules/.bin:/Users/robin/Sites/node_modules/.bin:/Users/robin/node_modules/.bin:/Users/node_modules/.bin:/node_modules/.bin:/usr/local/lib/node_modules/npm/node_modules/@npmcli/run-script/lib/node-gyp-bin:/Library/Frameworks/Python.framework/Versions/3.10/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/robin/Sites/mc-meditation-player/node_modules/.bin";
-
-/** @type {import('$env/static/private').TERMINAL_EMULATOR} */
-export const TERMINAL_EMULATOR = "JetBrains-JediTerm";
+export const PATH = "/Users/robin/Sites/mc-meditation-player/node_modules/.bin:/Users/robin/Sites/node_modules/.bin:/Users/robin/node_modules/.bin:/Users/node_modules/.bin:/node_modules/.bin:/usr/local/lib/node_modules/npm/node_modules/@npmcli/run-script/lib/node-gyp-bin:/Library/Frameworks/Python.framework/Versions/3.10/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin";
 
 /** @type {import('$env/static/private').npm_package_json} */
 export const npm_package_json = "/Users/robin/Sites/mc-meditation-player/package.json";
 
 /** @type {import('$env/static/private')._} */
 export const _ = "/Users/robin/Sites/mc-meditation-player/node_modules/.bin/vite";
+
+/** @type {import('$env/static/private').LaunchInstanceID} */
+export const LaunchInstanceID = "C40B3AC5-2FBC-4D1A-8CDC-0D7799AB452F";
 
 /** @type {import('$env/static/private').npm_config_userconfig} */
 export const npm_config_userconfig = "/Users/robin/.npmrc";
@@ -84,7 +81,7 @@ export const npm_config_userconfig = "/Users/robin/.npmrc";
 export const npm_config_init_module = "/Users/robin/.npm-init.js";
 
 /** @type {import('$env/static/private').__CFBundleIdentifier} */
-export const __CFBundleIdentifier = "com.jetbrains.WebStorm";
+export const __CFBundleIdentifier = "com.apple.Terminal";
 
 /** @type {import('$env/static/private').npm_command} */
 export const npm_command = "run-script";
@@ -101,6 +98,9 @@ export const EDITOR = "vi";
 /** @type {import('$env/static/private').npm_package_name} */
 export const npm_package_name = "sveltestarter";
 
+/** @type {import('$env/static/private').LANG} */
+export const LANG = "en_GB.UTF-8";
+
 /** @type {import('$env/static/private').XPC_FLAGS} */
 export const XPC_FLAGS = "0x0";
 
@@ -113,11 +113,11 @@ export const npm_package_version = "0.0.1";
 /** @type {import('$env/static/private').XPC_SERVICE_NAME} */
 export const XPC_SERVICE_NAME = "0";
 
-/** @type {import('$env/static/private').SHLVL} */
-export const SHLVL = "2";
-
 /** @type {import('$env/static/private').HOME} */
 export const HOME = "/Users/robin";
+
+/** @type {import('$env/static/private').SHLVL} */
+export const SHLVL = "2";
 
 /** @type {import('$env/static/private').npm_config_cache} */
 export const npm_config_cache = "/Users/robin/.npm";
@@ -131,11 +131,11 @@ export const LOGNAME = "robin";
 /** @type {import('$env/static/private').npm_lifecycle_script} */
 export const npm_lifecycle_script = "vite dev \"--open\"";
 
-/** @type {import('$env/static/private').LC_CTYPE} */
-export const LC_CTYPE = "en_GB.UTF-8";
-
 /** @type {import('$env/static/private').npm_config_user_agent} */
-export const npm_config_user_agent = "npm/8.1.0 node/v16.13.0 darwin arm64 workspaces/false";
+export const npm_config_user_agent = "npm/8.1.0 node/v16.13.0 darwin x64 workspaces/false";
+
+/** @type {import('$env/static/private').SECURITYSESSIONID} */
+export const SECURITYSESSIONID = "186a4";
 
 /** @type {import('$env/static/private').npm_node_execpath} */
 export const npm_node_execpath = "/usr/local/bin/node";
